b0cfa1d98892b01f39f44edf7cd93960be4101d4
mikeyo241 <mgardne1@tctc.edu>
2017-06-16 23:52:41 -0400

    Twig Templating and using blocks
    - Started using the default Symphony html template.
    - Finished up to Section 8 on KNP University.

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -10,15 +10,26 @@ namespace AppBundle\Controller;
 
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
-class GenusController
+class GenusController extends Controller
 {
   /**
    * @Route("/genus/{genusName}")
    */
   public function showAction($genusName)
   {
-    return new Response('The genus: ' . $genusName);
+    $notes = [
+      'Octopus asked me a riddle, outsmarted me',
+      'I counted 8 legs... as they wrapped around me',
+      'Inked!'
+    ];
+    return $this->render('genus/show.html.twig', [
+      'name' => $genusName,
+      'notes' => $notes
+    ]);
+
+
   }
 }
